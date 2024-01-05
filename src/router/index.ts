8f98730d369de9b63bda5828dc6cd00c46b11f0a
Waite Wang <waite@waite.wang>
2024-01-05 22:11:27 +0800

    ✨ feat: 页面整体新建以及路由完善

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,74 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    component: () => import('../views/UserLogin.vue')
-  },
-  {
-    path: '/index',
-    component: () => import('../views/HomeIndex.vue')
-  }
-]
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/login',
+      redirect: '/'
+    },
+    {
+      path: '/',
+      name: '登录',
+      meta: { title: '登录' },
+      component: () => import('../views/UserLogin.vue')
+    },
+    {
+      path: '/index',
+      name: '首页',
+      meta: { title: '首页' },
+      component: () => import('../views/IndexView.vue'),
+      children: [
+        {
+          path: '',
+          name: '主页',
+          meta: { title: '主页' },
+          component: () => import('../views/HomeView.vue')
+        },
+        {
+          path: 'mine',
+          name: '个人中心',
+          meta: { title: '个人中心' },
+          component: () => import('../views/user/MineView.vue')
+        },
+        {
+          path: 'setPwd',
+          name: '修改密码',
+          meta: { title: '修改密码' },
+          component: () => import('../views/user/SetPwdView.vue')
+        },
+        {
+          path: 'role',
+          name: '角色管理',
+          meta: { title: '角色管理' },
+          component: () => import('../views/user/RoleView.vue')
+        },
+        {
+          path: 'user',
+          name: '用户管理',
+          meta: { title: '用户管理' },
+          component: () => import('../views/user/UserView.vue')
+        },
+        {
+          path: 'roomType',
+          name: '房型管理',
+          meta: { title: '房型管理' },
+          component: () => import('../views/room/RoomTypeView.vue')
+        },
+        {
+          path: 'room',
+          name: '房间管理',
+          meta: { title: '房间管理' },
+          component: () => import('../views/room/RoomView.vue')
+        },
+        {
+          path: 'liveIn',
+          name: '入住用户',
+          meta: { title: '入住用户' },
+          component: () => import('../views/custom/LiveInView.vue')
+        }
+      ]
+    }
+  ]
 })
-
 export default router
--- src/views/HomeIndex.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="container position-sticky z-index-sticky top-0">
-    <div class="row">
-      <h1>adfad</h1>
-    </div>
-  </div>
-</template>
